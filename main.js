e31f2102d4f86a7eb474618d3ca03062f083519d
Stefan Kuznetsov <skuznetsov2@my.bcit.ca>
2020-09-23 16:56:20 -0700

    Added a banner

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
+// Defer until all the html elements are loaded
 window.onload = () => {
     const toggle = document.querySelector('#toggle')
+    const icon = toggle.querySelector('a')
     const menu = document.querySelectorAll('.item')
 
+    const barHtml = '<i class="fas fa-bars"></i>'
+
+    const lineHtml = '<i class="fas fa-grip-lines"></i>'
+
+    // Adds or removes the active class to every .item element
     toggle.addEventListener('click', () => {
-        [...menu].map(item => {
+        menu.forEach(item => {
             if (item.classList.contains('active')){
                 item.classList.remove('active')
+                icon.innerHTML = barHtml
             }
             else{
                 item.classList.add('active')
+                icon.innerHTML = lineHtml
             }
         })
     })
