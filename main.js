44c725f94e49fbb16610cdf012a607d3c0f40e36
Steven <clai86@my.bcit.ca>
2020-09-23 22:48:37 -0700

    clean up the code, modify navbar, and change the overall style

--- main.js
+++ main.js
@@ -1,25 +1,23 @@
 // Defer until all the html elements are loaded
 window.onload = () => {
-    const toggle = document.querySelector('#toggle')
-    const icon = toggle.querySelector('a')
-    const menu = document.querySelectorAll('.item')
+    const toggle = document.querySelector('#toggle');
+    const icon = toggle.querySelector('a');
+    const menu = document.querySelectorAll('.item');
 
-    const barHtml = '<i class="fas fa-bars"></i>'
+    const barHtml = '<i class="fas fa-bars"></i>';
 
-    const lineHtml = '<i class="fas fa-grip-lines"></i>'
+    const lineHtml = '<i class="fas fa-times"></i>';
 
     // Adds or removes the active class to every .item element
     toggle.addEventListener('click', () => {
-        menu.forEach(item => {
-            if (item.classList.contains('active')){
-                item.classList.remove('active')
-                icon.innerHTML = barHtml
+        menu.forEach((item) => {
+            if (item.classList.contains('active')) {
+                item.classList.remove('active');
+                icon.innerHTML = barHtml;
+            } else {
+                item.classList.add('active');
+                icon.innerHTML = lineHtml;
             }
-            else{
-                item.classList.add('active')
-                icon.innerHTML = lineHtml
-            }
-        })
-    })
-
-}
+        });
+    });
+};
